807ae5413d612361b9ed0e904142124156bffd73
Kivo07 <93500087+Kivo07@users.noreply.github.com>
2021-11-12 14:03:51 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+# This is a pythagorean theorem calculator.
+# The only difference is that it finds the length of a missing side.
+
+import math
+
+a2 = 7**2
+b2 = 0
+c2 = 10**2
+
+print(a2)
+print(c2)
+b2 = c2 - a2
+
+print(b2)
+
+print(math.sqrt(b2))
+
+print("the answer is:")
+print(math.sqrt(b2))
+
+
